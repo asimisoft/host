0007e54cde5e8f307010e93caa5440fe349e5973
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-05-18 22:50:36 +0545

    scaffold generated

---
+++ db/migrate/20170518162848_create_microposts.rb
@@ -0,0 +1,10 @@
+class CreateMicroposts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :microposts do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
